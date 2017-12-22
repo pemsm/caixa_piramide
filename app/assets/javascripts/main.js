2fe6311f8f7646a486e8ddd913d6ad05be801b97
pablomaribondo <pablomgvieira@gmail.com>
2017-12-22 17:06:18 +0000

    -minor fixes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,7 +2,7 @@ function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("tabela-caixa");
   switching = true;
-  dir = "asc"; 
+  dir = "asc";
   while (switching) {
     switching = false;
     rows = table.getElementsByTagName("TR");
@@ -10,8 +10,10 @@ function sortTable(n) {
       shouldSwitch = false;
       x = rows[i].getElementsByTagName("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
+
       var prevToFloat = parseFloat(x.innerHTML.toLowerCase());
       var nextToFloat = parseFloat(y.innerHTML.toLowerCase());
+
       if (dir == "asc") {
         if (prevToFloat > nextToFloat) {
           shouldSwitch= true;
@@ -27,7 +29,7 @@ function sortTable(n) {
     if (shouldSwitch) {
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      switchcount ++; 
+      switchcount ++;
     } else {
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
@@ -37,25 +39,78 @@ function sortTable(n) {
   }
 }
 
+/*function sortTableDate(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("tabela-caixa");
+  switching = true;
+  dir = "asc";
+  while (switching) {
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+
+    var teste = {
+      item: function(teste2) {
+        return {
+          dia: teste2[0],
+          mes: teste2[1],
+          ano: teste2[2]
+        }
+      }
+
+    };
+    var teste2 = rows[1].getElementsByTagName("TD")[n].innerHTML.split('/');
+    var myJSON = JSON.stringify(teste['teste2']);
+    console.log(teste['item']['dia']);
+
+    
+    for (i = 1; i < (rows.length - 2); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+
+      var prevToFloat = parseFloat(x.innerHTML.toLowerCase());
+      var nextToFloat = parseFloat(y.innerHTML.toLowerCase());
+
+      var xsplit = x.innerHTML.split('/');
+      var ysplit = y.innerHTML.split('/'); 
+
+      if (dir == "asc") {
+        if (prevToFloat > nextToFloat) {
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (prevToFloat < nextToFloat) {
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount ++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}*/
+
 function openTab(event, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  
+
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  
+
   document.getElementById(tabName).style.display = "block";
   event.currentTarget.className += " active";
 }
-
-
-
-
-
-
-
