88a6a2965c6c3af6916650e76358e787220e0914
pablomaribondo <pablomgvieira@gmail.com>
2017-12-24 00:27:57 +0000

    -implementando barra de buscas e ícones nos botões que tão sem

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,25 +1,24 @@
+// SORT TABLE
 function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, direction, switchcount = 0;
-  table = document.getElementById("tabela-caixa");
+  var table, rows, switching, i, prev, next, shouldSwitch, direction, switchcount = 0;
+  var prevToFloat = parseFloat(prev.innerHTML.toLowerCase());
+  var nextToFloat = parseFloat(next.innerHTML.toLowerCase());
+  table = document.getElementById('tabela-caixa');
   switching = true;
-  direction = "asc";
+  direction = 'asc';
   while (switching) {
     switching = false;
-    rows = table.getElementsByTagName("tr");
+    rows = table.getElementsByTagName('tr');
     for (i = 1; i < (rows.length - 2); i++) {
       shouldSwitch = false;
-      x = rows[i].getElementsByTagName("td")[n];
-      y = rows[i + 1].getElementsByTagName("td")[n];
-      
-      var prevToFloat = parseFloat(x.innerHTML.toLowerCase());
-      var nextToFloat = parseFloat(y.innerHTML.toLowerCase());
-      
-      if (direction == "asc") {
+      prev = rows[i].getElementsByTagName('td')[n];
+      next = rows[i + 1].getElementsByTagName('td')[n];
+      if (direction == 'asc') {
         if (prevToFloat > nextToFloat) {
           shouldSwitch= true;
           break;
         }
-      } else if (direction == "desc") {
+      } else if (direction == 'desc') {
         if (prevToFloat < nextToFloat) {
           shouldSwitch= true;
           break;
@@ -31,22 +30,22 @@ function sortTable(n) {
       switching = true;
       switchcount ++;
     } else {
-      if (switchcount == 0 && direction == "asc") {
-        direction = "desc";
+      if (switchcount == 0 && direction == 'asc') {
+        direction = 'desc';
         switching = true;
       }
     }
   }
 }
-// SORT DATE: BEGINNING
+// SORT DATE
 function convertDate(date) {
-  var splitDate = date.split("/");
+  var splitDate = date.split('/');
   return (splitDate[2]+splitDate[1]+splitDate[0]);
 }
 
 function sortDateAsc() {
-  var tbody = document.querySelector("#tabela-caixa tbody");
-  var rows = [].slice.call(tbody.querySelectorAll("tr"));
+  var tbody = document.querySelector('#tabela-caixa tbody');
+  var rows = [].slice.call(tbody.querySelectorAll('tr'));
   
   rows.sort(function(a,b) {
     return convertDate(a.cells[0].innerHTML) - convertDate(b.cells[0].innerHTML);
@@ -57,8 +56,8 @@ function sortDateAsc() {
 }
 
 function sortDateDesc() {
-  var tbody = document.querySelector("#tabela-caixa tbody");
-  var rows = [].slice.call(tbody.querySelectorAll("tr"));
+  var tbody = document.querySelector('#tabela-caixa tbody');
+  var rows = [].slice.call(tbody.querySelectorAll('tr'));
   
   rows.sort(function(a,b) {
     return convertDate(b.cells[0].innerHTML) - convertDate(a.cells[0].innerHTML);
@@ -79,26 +78,48 @@ function sortTableByDate() {
   return counter += 1;
 }
 
-window.onload = function () {
-  sortTable(1);
-  sortTable(1);
-  document.querySelector("#tabela-caixa th").click();
-}
-
-// SORT DATE: END
+// TAB
 
 function openTab(event, tabName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
   
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
   
-  document.getElementById(tabName).style.display = "block";
-  event.currentTarget.className += " active";
+  document.getElementById(tabName).style.display = 'block';
+  event.currentTarget.className += ' active';
+}
+
+// SEARCH
+
+function searchDate() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  table = document.getElementById('tabela-caixa');
+  tr = table.getElementsByTagName('tr');
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
+    } 
+  }
+}
+
+
+window.onload = function () {
+  sortTable(1);
+  sortTable(1);
+  document.querySelector('#tabela-caixa th').click();
 }
